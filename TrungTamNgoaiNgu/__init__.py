d6b72be13639b52f03b0733040b9f554a6dc9bc3
phanlongnhatlam <nhatlamphan2005@gmail.com>
2025-12-21 16:50:51 +0700

    hi

--- TrungTamNgoaiNgu/__init__.py
+++ TrungTamNgoaiNgu/__init__.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 app.secret_key = "^%^&%^(*^^&^&^(*^&^&^&^"
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:abcd@localhost/trungtamngoaingu_db?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config["PAGE_SIZE"] = 20
+app.config["PAGE_SIZE"] = 10
 db = SQLAlchemy(app)
 login = LoginManager(app)
